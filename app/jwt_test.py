08274c7b92ff6ee74078bd03476215534ddc0acf
meshnesh <tonnie.nesh@gmail.com>
2017-12-26 19:06:14 +0300

    Implement JWT in 'jwt_test.py' file

--- app/jwt_test.py
+++ app/jwt_test.py
@@ -1,15 +1,40 @@
-from flask import Flask
-from flask_restful import Resource, Api
+# -*- coding: utf-8 -*-
+"""import depancies."""
+import re
+from flask import Flask, abort
+from flask_restful import reqparse, Resource, Api, Resource, fields, marshal
+from flasgger import Swagger
 from flask_jwt import JWT, jwt_required
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret'
 api = Api(app, prefix="/api/v1")
 
 USER_DATA = {
-    "masnun": "abc123"
+    "john.D@gmail.com": "qwerty1234"
 }
 
+USERS = [
+    {
+        'id':1,
+        'name': u'John Doe',
+        'email': u'john.D@gmail.com',
+        'password': u'qwerty1234'
+    },
+    {
+        'id':2,
+        'name': u'Mary Jane',
+        'email': u'jane.mary@yahoo.com',
+        'password': u'qwerty1234'
+    },
+    {
+        'id':3,
+        'name': u'Antony Ng\'ang\'a',
+        'email': u'tonny.nesh@gmail.com',
+        'password': u'qwerty1234'
+    }
+]
 
 class User(object):
     def __init__(self, id):
@@ -19,11 +44,16 @@ class User(object):
         return "User(id='%s')" % self.id
 
 
-def verify(username, password):
-    if not (username and password):
+def verify(email, password):
+    if not (email and password):
+        print(password)
         return False
-    if USER_DATA.get(username) == password:
-        return User(id=123)
+    for user in USERS:
+        if user['email'] == email:
+            print(email)
+            if user['password'] == password:
+                print(password)
+                return User(id=123)
 
 def identity(payload):
     user_id = payload['identity']
