677eb49f78b60b201082b1536df223dc839679ba
meshnesh <tonnie.nesh@gmail.com>
2018-01-05 02:49:05 +0300

    Implement 'user-login' tests in 'test_auth.py'

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -42,3 +42,31 @@ class AuthTestCase(unittest.TestCase):
         result = json.loads(second_res.data.decode())
         self.assertEqual(
             result['message'], "User already exists. Please login.")
+
+    def test_user_login(self):
+        """Test registered user can login."""
+        res = self.client().post('/api/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client().post('/api/auth/login', data=self.user_data)
+        result = json.loads(login_res.data.decode())
+        self.assertEqual(result['message'], "You logged in successfully.")
+        self.assertEqual(login_res.status_code, 200)
+        self.assertTrue(result['access_token'])
+
+    def test_non_registered_user_login(self):
+        """Test non registered users cannot login."""
+        # define a dictionary to represent an unregistered user
+        not_a_user = {
+            'email': 'not_a_user@example.com',
+            'password': 'nope'
+        }
+        # send a POST request to /auth/login with the data above
+        res = self.client().post('/api/auth/login', data=not_a_user)
+        # get the result in json
+        result = json.loads(res.data.decode())
+
+        # assert that this response must contain an error message 
+        # and an error status code 401(Unauthorized)
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(
+            result['message'], "Invalid email or password, Please try again")
