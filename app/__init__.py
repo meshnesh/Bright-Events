d840ed67fb7e85b2384ac45f8d4b1fd2f00b8608
meshnesh <tonnie.nesh@gmail.com>
2018-01-11 14:28:08 +0300

    Implement api documentation

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask import request, jsonify, abort, make_response
 
+
 # local import
 from instance.config import app_config
 
--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 
 from app import create_app
+from flasgger import Swagger
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
+
+Swagger(app, template_file="docs/docs.yml")
 
 if __name__ == '__main__':
     app.run()
