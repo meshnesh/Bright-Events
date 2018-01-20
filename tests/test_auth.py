f3c12d05773631b5d0069303b618ac88710262ef
meshnesh <tonnie.nesh@gmail.com>
2018-01-20 21:56:44 +0300

    [chore] Remove unused imports

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,7 +3,6 @@
 import unittest
 import json
 from app import create_app, db
-from app.models import BlacklistToken
 
 class AuthTestCase(unittest.TestCase):
     """Test case for the authentication blueprint."""
