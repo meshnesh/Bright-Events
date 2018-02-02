08650d0711055db17b1805223be037ac822f76fd
meshnesh <tonnie.nesh@gmail.com>
2018-02-02 23:48:39 +0300

    [Chore] Update code to pylint standard

--- app/auth/views.py
+++ app/auth/views.py
@@ -35,7 +35,12 @@ class RegistrationView(MethodView):
                 email = post_data['email']
                 password = post_data['password']
                 email_confirmed = False
-                user = User(name=name, email=email, password=password, email_confirmed=email_confirmed)
+
+                user = User(
+                    name=name, email=email, password=password,
+                    email_confirmed=email_confirmed
+                )
+
                 email_match = re.match(EMAIL_VALIDATOR, user.email)
 
                 if not email_match:
