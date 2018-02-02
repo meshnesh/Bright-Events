48146682eb6a01b3d4cea5cfbf4a77608e390a09
meshnesh <tonnie.nesh@gmail.com>
2018-02-02 18:26:11 +0300

    [Chore] Remove flask mail import

--- run.py
+++ run.py
@@ -3,11 +3,9 @@
 import os
 
 from app import create_app
-from flask_mail import Mail
 
-CONFIG_NAME = os.getenv('APP_SETTINGS') # config_name = "development"
+CONFIG_NAME = os.getenv('APP_SETTINGS')
 APP = create_app(CONFIG_NAME)
-MAIL = Mail(APP)
 
 if __name__ == '__main__':
     APP.run()
