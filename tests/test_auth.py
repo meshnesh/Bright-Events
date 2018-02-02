acb8db191642304584865543c22685061782921e
meshnesh <tonnie.nesh@gmail.com>
2018-02-02 13:35:04 +0300

    [chore] uncommented the code

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,198 +1,198 @@
-# """import depancies."""
+"""import depancies."""
 
-# import unittest
-# import json
-# from app import create_app, db
+import unittest
+import json
+from app import create_app, db
 
-# class AuthTestCase(unittest.TestCase):
-#     """Test case for the authentication blueprint."""
+class AuthTestCase(unittest.TestCase):
+    """Test case for the authentication blueprint."""
 
-#     def setUp(self):
-#         """Set up test variables."""
-#         self.app = create_app(config_name="testing")
-#         # initialize the test client
-#         self.client = self.app.test_client
+    def setUp(self):
+        """Set up test variables."""
+        self.app = create_app(config_name="testing")
+        # initialize the test client
+        self.client = self.app.test_client
 
-#         with self.app.app_context():
-#             # create all tables
-#             db.session.close()
-#             db.drop_all()
-#             db.create_all()
+        with self.app.app_context():
+            # create all tables
+            db.session.close()
+            db.drop_all()
+            db.create_all()
 
-#     def user_registration(self):
-#         """This helper method helps register a test user."""
-#         user_data = {
-#             'name':'test user',
-#             'email': 'test@example.com',
-#             'password': 'test_password'
-#         }
-#         return self.client().post('/api/auth/register', data=user_data)
+    def user_registration(self):
+        """This helper method helps register a test user."""
+        user_data = {
+            'name':'test user',
+            'email': 'test@example.com',
+            'password': 'test_password'
+        }
+        return self.client().post('/api/auth/register', data=user_data)
 
-#     def user_login(self):
-#         """This helper method helps log in a test user."""
-#         user_data = {
-#             'email': 'test@example.com',
-#             'password': 'test_password'
-#         }
-#         return self.client().post('/api/auth/login', data=user_data)
+    def user_login(self):
+        """This helper method helps log in a test user."""
+        user_data = {
+            'email': 'test@example.com',
+            'password': 'test_password'
+        }
+        return self.client().post('/api/auth/login', data=user_data)
 
-#     def get_access_token(self):
-#         """register and login a user to get an access token"""
-#         self.user_registration()
-#         result = self.user_login()
-#         access_token = json.loads(result.data.decode())['access_token']
-#         return access_token
+    def get_access_token(self):
+        """register and login a user to get an access token"""
+        self.user_registration()
+        result = self.user_login()
+        access_token = json.loads(result.data.decode())['access_token']
+        return access_token
 
-#     def user_email_reset(self):
-#         """This helper method helps check user email to reset password."""
-#         user_data = {
-#             'email': 'test@example.com',
-#             'password': 'test_password'
-#         }
-#         return self.client().post('/api/auth/reset', data=user_data)
+    def user_email_reset(self):
+        """This helper method helps check user email to reset password."""
+        user_data = {
+            'email': 'test@example.com',
+            'password': 'test_password'
+        }
+        return self.client().post('/api/auth/reset', data=user_data)
 
-#     def user_logout(self):
-#         """This helper method helps log out a test user."""
-#         access_token = self.get_access_token()
+    def user_logout(self):
+        """This helper method helps log out a test user."""
+        access_token = self.get_access_token()
 
-#         response = self.client().post(
-#             '/api/auth/logout',
-#             headers=dict(
-#                 Authorization='Bearer ' + access_token)
-#         )
+        response = self.client().post(
+            '/api/auth/logout',
+            headers=dict(
+                Authorization='Bearer ' + access_token)
+        )
 
-#         return response
+        return response
 
-#     def test_registration(self):
-#         """Test user registration works correcty."""
-#         register_user = self.user_registration()
-#         self.assertEqual(register_user.status_code, 201)
+    def test_registration(self):
+        """Test user registration works correcty."""
+        register_user = self.user_registration()
+        self.assertEqual(register_user.status_code, 201)
 
-#         result = json.loads(register_user.data.decode())
-#         self.assertEqual(
-#             result['message'], "You registered successfully. Please log in.")
+        result = json.loads(register_user.data.decode())
+        self.assertEqual(
+            result['message'], "You registered successfully. Please log in.")
 
-#     def test_already_registered_user(self):
-#         """Test that a user cannot be registered twice."""
-#         self.user_registration() # register a user
-#         second_res = self.user_registration() # re-register the same user
-#         self.assertEqual(second_res.status_code, 202)
-#         # get the results returned in json format
-#         result = json.loads(second_res.data.decode())
-#         self.assertEqual(
-#             result['message'], "User already exists. Please login.")
+    def test_already_registered_user(self):
+        """Test that a user cannot be registered twice."""
+        self.user_registration() # register a user
+        second_res = self.user_registration() # re-register the same user
+        self.assertEqual(second_res.status_code, 202)
+        # get the results returned in json format
+        result = json.loads(second_res.data.decode())
+        self.assertEqual(
+            result['message'], "User already exists. Please login.")
 
-#     def test_user_login(self):
-#         """Test registered user can login."""
-#         self.user_registration()
-#         login_user = self.user_login()
-#         self.assertEqual(login_user.status_code, 200)
+    def test_user_login(self):
+        """Test registered user can login."""
+        self.user_registration()
+        login_user = self.user_login()
+        self.assertEqual(login_user.status_code, 200)
 
-#     def test_non_registered_user_login(self):
-#         """Test non registered users cannot login."""
-#         # define a dictionary to represent an unregistered user
-#         not_a_user = {
-#             'email': 'not_a_user@example.com',
+    def test_non_registered_user_login(self):
+        """Test non registered users cannot login."""
+        # define a dictionary to represent an unregistered user
+        not_a_user = {
+            'email': 'not_a_user@example.com',
+            'password': 'nope'
+        }
+        # send a POST request to /auth/login with the data above
+        res = self.client().post('/api/auth/login', data=not_a_user)
+        # get the result in json
+        result = json.loads(res.data.decode())
+
+        # assert that this response must contain an error message
+        # and an error status code 401(Unauthorized)
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(
+            result['message'], "Invalid email or password, Please try again")
+
+    def test_email_exist_for_reset(self):
+        """Test Email exists so that they can reset there password"""
+        self.user_registration()
+        res = self.user_email_reset()
+
+        result = json.loads(res.data.decode())
+        self.assertEqual(result['message'], "Check your email to reset your password.")
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_email_non_exist_for_reset(self):
+        """Test not registered Email fails when reseting there password"""
+        self.user_registration() # register a user
+
+        not_a_user = {
+            'email': 'not_a_user@example.com'
+        }
+
+        reset_res = self.client().post(
+            '/api/auth/reset', data=not_a_user
+        ) # make request with a none existence user
+
+        result = json.loads(reset_res.data.decode())
+        self.assertEqual(result['message'], "Wrong Email or user email does not exist.")
+        self.assertEqual(reset_res.status_code, 401)
+
+# # FIXED WHEN EMAILING IS SETUP
+#     def test_user_reset_password(self):
+#         """Test Email exists so that they can reset there password"""
+#         new_password = {
 #             'password': 'nope'
 #         }
-#         # send a POST request to /auth/login with the data above
-#         res = self.client().post('/api/auth/login', data=not_a_user)
-#         # get the result in json
-#         result = json.loads(res.data.decode())
 
-#         # assert that this response must contain an error message
-#         # and an error status code 401(Unauthorized)
-#         self.assertEqual(res.status_code, 401)
-#         self.assertEqual(
-#             result['message'], "Invalid email or password, Please try again")
-
-#     def test_email_exist_for_reset(self):
-#         """Test Email exists so that they can reset there password"""
 #         self.user_registration()
 #         res = self.user_email_reset()
 
-#         result = json.loads(res.data.decode())
-#         self.assertEqual(result['message'], "Check your email to reset your password.")
-#         self.assertEqual(res.status_code, 200)
+#         # result = json.loads(res.data.decode())
+#         # self.assertEqual(result['message'], "Check your email to reset your password.")
+#         # self.assertEqual(res.status_code, 200)
+#         # self.assertTrue(result['access_token'])
 
+#         # obtain the access token
+#         access_token = json.loads(res.data.decode())['access_token']
 
-#     def test_email_non_exist_for_reset(self):
-#         """Test not registered Email fails when reseting there password"""
-#         self.user_registration() # register a user
+#         password_res = self.client().put(
+#             '/api/auth/reset-password',
+#             headers=dict(Authorization="Bearer " + access_token),
+#             data=new_password)
+#         self.assertEqual(password_res.status_code, 201)
 
-#         not_a_user = {
-#             'email': 'not_a_user@example.com'
-#         }
+    def test_user_status(self):
+        """ Test for user status """
+        access_token = self.get_access_token()
 
-#         reset_res = self.client().post(
-#             '/api/auth/reset', data=not_a_user
-#         ) # make request with a none existence user
+        response = self.client().get(
+            '/api/auth/status',
+            headers=dict(
+                Authorization='Bearer ' + access_token)
+        )
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'success')
+        self.assertTrue(data['data'] is not None)
+        self.assertEqual(response.status_code, 200)
 
-#         result = json.loads(reset_res.data.decode())
-#         self.assertEqual(result['message'], "Wrong Email or user email does not exist.")
-#         self.assertEqual(reset_res.status_code, 401)
+    def test_valid_logout(self):
+        """ Test for logout before token expires """
+        response = self.user_logout()
 
-# # # FIXED WHEN EMAILING IS SETUP
-# #     def test_user_reset_password(self):
-# #         """Test Email exists so that they can reset there password"""
-# #         new_password = {
-# #             'password': 'nope'
-# #         }
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'success')
+        self.assertTrue(data['message'] == 'Successfully logged out.')
+        self.assertEqual(response.status_code, 200)
 
-# #         self.user_registration()
-# #         res = self.user_email_reset()
+    def test_invalid_logout(self):
+        """ Test for logout after the token expires """
+        self.user_logout() # valid token logout
+        response = self.user_logout() # secondary logout
 
-# #         # result = json.loads(res.data.decode())
-# #         # self.assertEqual(result['message'], "Check your email to reset your password.")
-# #         # self.assertEqual(res.status_code, 200)
-# #         # self.assertTrue(result['access_token'])
+        self.assertEqual(response.status_code, 401)
 
-# #         # obtain the access token
-# #         access_token = json.loads(res.data.decode())['access_token']
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
 
-# #         password_res = self.client().put(
-# #             '/api/auth/reset-password',
-# #             headers=dict(Authorization="Bearer " + access_token),
-# #             data=new_password)
-# #         self.assertEqual(password_res.status_code, 201)
-
-#     def test_user_status(self):
-#         """ Test for user status """
-#         access_token = self.get_access_token()
-
-#         response = self.client().get(
-#             '/api/auth/status',
-#             headers=dict(
-#                 Authorization='Bearer ' + access_token)
-#         )
-#         data = json.loads(response.data.decode())
-#         self.assertTrue(data['status'] == 'success')
-#         self.assertTrue(data['data'] is not None)
-#         self.assertEqual(response.status_code, 200)
-
-#     def test_valid_logout(self):
-#         """ Test for logout before token expires """
-#         response = self.user_logout()
-
-#         data = json.loads(response.data.decode())
-#         self.assertTrue(data['status'] == 'success')
-#         self.assertTrue(data['message'] == 'Successfully logged out.')
-#         self.assertEqual(response.status_code, 200)
-
-#     def test_invalid_logout(self):
-#         """ Test for logout after the token expires """
-#         self.user_logout() # valid token logout
-#         response = self.user_logout() # secondary logout
-
-#         self.assertEqual(response.status_code, 401)
-
-#     def tearDown(self):
-#         """teardown all initialized variables."""
-#         with self.app.app_context():
-#             # drop all tables
-#             db.session.remove()
-#             db.drop_all()
-
-# # Make the tests conveniently executable
-# if __name__ == "__main__":
-#     unittest.main()
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
