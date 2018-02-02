58efc9f34d97107fdb4b7a89278b4098f936ba85
meshnesh <tonnie.nesh@gmail.com>
2018-02-02 15:33:52 +0300

    [chore] update email config

--- instance/config.py
+++ instance/config.py
@@ -7,6 +7,12 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = 'tonnie.nesh'
+    MAIL_PASSWORD = 'teteteteateher'
 
 
 class DevelopmentConfig(Config):
