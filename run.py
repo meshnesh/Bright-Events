1f71932e165094936ae6c83c05862227ae3bdbe7
meshnesh <tonnie.nesh@gmail.com>
2018-02-02 17:16:53 +0300

    [Bug] Remove code creating travis bug

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import os
 
 from app import create_app
-from flask_mail import Mail
 
 CONFIG_NAME = os.getenv('APP_SETTINGS') # config_name = "development"
 APP = create_app(CONFIG_NAME)
-MAIL = Mail(APP)
 
 if __name__ == '__main__':
     APP.run()
