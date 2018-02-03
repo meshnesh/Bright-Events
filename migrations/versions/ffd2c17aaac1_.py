13c6aa67a0708e16ef7c08f67c55b641a1a3ad0f
meshnesh <tonnie.nesh@gmail.com>
2018-02-04 01:05:09 +0300

    [bug] fix migrations error

--- migrations/versions/ffd2c17aaac1_.py
+++ migrations/versions/ffd2c17aaac1_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 019e5200cb79
+Revision ID: ffd2c17aaac1
 Revises: 
-Create Date: 2018-02-02 23:39:24.814607
+Create Date: 2018-02-03 23:40:59.166442
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '019e5200cb79'
+revision = 'ffd2c17aaac1'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -38,7 +38,7 @@ def upgrade():
     sa.Column('name', sa.String(length=15), nullable=False),
     sa.Column('email', sa.String(length=25), nullable=False),
     sa.Column('password', sa.String(length=100), nullable=True),
-    sa.Column('email_confirmed', sa.Boolean(create_constraint=False), nullable=False),
+    sa.Column('email_confirmed', sa.Boolean(create_constraint=False), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email')
     )
