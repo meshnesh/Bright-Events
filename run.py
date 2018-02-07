302553ffaec805dbc9d7d09d9f5c3b6fe3bc62c6
meshnesh <tonnie.nesh@gmail.com>
2018-02-08 00:59:45 +0300

    [Chore] Remove unnecessary code and imports

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 """import depancies and methods."""
 
 import os
-
 from app import create_app
-from flask_mail import Mail
 
 CONFIG_NAME = os.getenv('APP_SETTINGS')
 APP = create_app(CONFIG_NAME)
-MAIL = Mail(APP)
 
 if __name__ == '__main__':
     APP.run()
