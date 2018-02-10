c423160175461187ffd69d3c11f9d04c733a8017
meshnesh <tonnie.nesh@gmail.com>
2018-02-10 19:04:39 +0300

    [Feature] Create decorator for signature

--- app/emails/__init__.py
+++ app/emails/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask_mail import Message
-from flask import current_app
+from flask import current_app, make_response, jsonify
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 from app import mail
 
@@ -15,8 +15,20 @@ def send_mail(to, subject, html):
 	mail.send(msg)
 	return "sent"
 
-def confirm_token(token, ss):
-	return SECRET.loads(
-		token, salt=ss,
-		max_age=3600 # token valid for 1 hour
-	)
+def confirm_token(salt):
+	def wrapper(f):
+		def inner(*args, **kwargs):
+			try:
+				email = SECRET.loads(
+					kwargs['token'], salt=salt,
+					max_age=3600 # token valid for 1 hour
+				) 
+				return f(*args, **kwargs, email=email)
+
+			except SignatureExpired:
+				response = {
+				'message': 'The token is expired!, confirm your e-mail again'
+				}
+				return make_response(jsonify(response)), 401
+		return inner
+	return wrapper
