68a2ad7d005558175ff3b449f9e1299413a35870
meshnesh <tonnie.nesh@gmail.com>
2018-02-10 17:59:20 +0300

    [Chore] Implement token and salt

--- app/emails/__init__.py
+++ app/emails/__init__.py
@@ -1,6 +1,10 @@
+import os
 from flask_mail import Message
 from flask import current_app
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 from app import mail
+
+SECRET = URLSafeTimedSerializer(os.getenv('SECRET'))
 
 def send_mail(to, subject, html):
 	"""a function to send emails from the app"""
@@ -10,3 +14,9 @@ def send_mail(to, subject, html):
 	msg.html = html
 	mail.send(msg)
 	return "sent"
+
+def confirm_token(token, ss):
+	return SECRET.loads(
+		token, salt=ss,
+		max_age=3600 # token valid for 1 hour
+	)
